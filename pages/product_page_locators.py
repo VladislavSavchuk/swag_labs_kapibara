db21413a039a54c156997d794a9ec1f263c5962f
Vladislav Savchuk <vladislav.savvchuk@gmail.com>
2024-08-01 23:37:48 +0300

    add folders and locators pages login and product

---
+++ pages/product_page_locators.py
@@ -0,0 +1,18 @@
+"""Product page locators"""
+
+
+class ProductPageLocators:
+    """The class contains locators to product page """
+    app_logo = '[class="app_logo"]'
+    burger_menu = '[id="react-burger-menu-btn"]'
+    shopping_cart_icon = '[id="shopping_cart_container"]'
+    product_sort = '[data-test="active-option"]'
+
+    # buttons
+    add_to_cart_backpack = '[id="add-to-cart-sauce-labs-backpack"]'
+    add_to_cart_light = '[id="add-to-cart-sauce-labs-bike-light"]'
+    add_to_cart_shirt = '[id="add-to-cart-sauce-labs-bolt-t-shirt"]'
+    add_to_cart_jacket = '[id="add-to-cart-sauce-labs-fleece-jacket"]'
+    add_to_cart_onesie = '[id="add-to-cart-sauce-labs-onesie"]'
+    add_to_cart_shirt_red = \
+        '[id="add-to-cart-test.allthethings()-t-shirt-(red)"]'
