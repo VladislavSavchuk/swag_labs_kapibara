810abfca8b9d9a103b99e0d8ba50a7c5f23b0fc0
Vladislav Savchuk <vladislav.savvchuk@gmail.com>
2024-08-11 12:24:06 +0300

    add login creds and constants

---
+++ test_data/login_creds.py
@@ -0,0 +1,10 @@
+"""Strings involving the login page. i.e. usernames and passwords"""
+
+# usernames
+TEST_USER = "standard_user"
+STANDARD_USER = "standard_user"
+PROBLEM_USER = "problem_user"
+LOCKED_OUT_USER = "locked_out_user"
+EMPTY_STRING = ""
+# passwords
+STANDARD_PASSWORD = "secret_sauce"
