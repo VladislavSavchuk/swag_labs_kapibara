3cad31eca3af46ba19ca14286eaee1c0802e813f
Vladislav Savchuk <vladislav.savvchuk@gmail.com>
2024-08-11 12:26:14 +0300

    add method open web-browser and file pytest.ini

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+"""This module contains the fixtures for the tests"""
+
+import pytest
+from selenium import webdriver
+from test_data.constants import URL
+
+
+@pytest.fixture()
+def driver():
+    """
+    Fixture that provides a WebDriver instance for the tests.
+
+    This fixture initializes the Chrome WebDriver with specified options,
+    sets an implicit wait time, and navigates to the predefined URL.
+    It ensures that the browser is opened before the test and properly
+    closed after the test.
+
+    Returns:
+        selenium.webdriver.Chrome: The WebDriver instance used
+        for browser interactions.
+    """
+    options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=1920,1080")
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(5)
+    browser.get(URL)
+    yield browser
+    browser.quit()
