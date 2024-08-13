188a9825fceb2f42607c53796b77cc5d4af548bb
Vladislav Savchuk <vladislav.savvchuk@gmail.com>
2024-08-13 22:54:45 +0300

    deleted unused variables in the creds

--- test_data/login_creds.py
+++ test_data/login_creds.py
@@ -1,9 +1,7 @@
 """Strings involving the login page. i.e. usernames and passwords"""
 
 # usernames
-TEST_USER = "standard_user"
 STANDARD_USER = "standard_user"
-PROBLEM_USER = "problem_user"
 LOCKED_OUT_USER = "locked_out_user"
 EMPTY_STRING = ""
 # passwords
