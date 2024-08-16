90b0c5e1a79495c1f002273e7a103fd86f98796a
Aksana.Batyrova <aksana.batyrova@ventionteams.com>
2024-08-16 14:14:29 +0300

    add test data for checkout and payment

---
+++ test_data/checkout_info.py
@@ -0,0 +1,9 @@
+"""Info used for checkout testing"""
+
+FIRST_NAME_VALID = "John"
+LAST_NAME_VALID = "Smith"
+POSTCODE_VALID = "123456"
+
+FIRST_NAME_INVALID = "J0#№"
+LAST_NAME_INVALID = "$M!~#"
+POSTCODE_INVALID = "!@#$%^"
---
+++ test_data/errors_text.py
@@ -0,0 +1,9 @@
+"""Errors text appear all over the app"""
+
+CHECKOUT_EMPTY_FIRST_NAME = 'Error: First Name is required'
+CHECKOUT_EMPTY_LAST_NAME = 'Error: Last Name is required'
+CHECKOUT_EMPTY_POSTCODE = 'Error: Postal Code is required'
+
+CHECKOUT_INVALID_FIRST_NAME = 'Error: First Name is invalid'
+CHECKOUT_INVALID_LAST_NAME = 'Error: Last Name is invalid'
+CHECKOUT_INVALID_POSTCODE = 'Error: Postal Code is invalid'
